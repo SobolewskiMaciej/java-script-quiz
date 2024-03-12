853b336d0823f80b75da630a97c1f32011c34bea
Maciej Sobolewski <sobolewski45@gmail.com>
2024-03-12 09:03:21 +0000

    create variables, logic.js

---
+++ assets/js/logic.js
@@ -0,0 +1,15 @@
+// Variables
+var startScreenSection = document.getElementById("start-screen");
+var startButton = document.getElementById("start");
+var timerOnDisplay = document.getElementById("time");
+var questionsSection = document.getElementById("questions");
+var questionTitle = document.getElementById("question-title");
+var questionChoices = document.getElementById("choices");
+var endScreenSection = document.getElementById("end-screen");
+var feedbackSection = document.getElementById("feedback");
+var submitButton = document.getElementById("submit");
+var initialsInputText = document.getElementById("initials");
+var finalScoreTextSection = document.getElementById("final-score");
+var timerCount = 75;
+var timer;
+var currentQuestionIndex = 0;
