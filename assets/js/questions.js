e5c756e9a0dbe9b7331838c8f743688913a38d66
Maciej Sobolewski <sobolewski45@gmail.com>
2024-03-12 08:59:44 +0000

    add questions js and update html

---
+++ assets/js/questions.js
@@ -0,0 +1,28 @@
+var quizQuestions = [
+    {
+      question: "Is London a Capital of?",
+      choices: ["Hungary", "Italy", "England", "France"],
+      answer: "England",
+    },
+    {
+      question: "Is 11111 greater than 1111?",
+      choices: ["yes", "no", "maybe", "i am hungry"],
+      answer: "yes",
+    },
+    {
+      question: "Is procrastination a good thing?",
+      choices: ["yes", "sometimes", "no, it is not", "ask your neighbour"],
+      answer: "no, it is not",
+    },
+    {
+      question: "Michael Jordan was a famous",
+      choices: ["racing driver", "fisherman", "cleaner", "basketball player"],
+      answer: "basketball player",
+    },
+    {
+      question: "How many legs does Homo Sapiens have?",
+      choices: ["4", "6", "2", "8"],
+      answer: "2",
+    },
+  ];
+  
